95ed2ed7c5ff495a7d25cec24feb15e685fab290
Raymond_Mannarelli <raymond0831@comcast.net>
2014-08-14 20:33:11 -0500

    Intentional Bug

--- lib/Acme/MyFirstModule/RAYMONDJM.pm
+++ lib/Acme/MyFirstModule/RAYMONDJM.pm
@@ -21,11 +21,11 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
-our @EXPORT = qw(
+our @EXPORT; = qw(
 	
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 # Preloaded methods go here.
